6285867910b990d314c947fe0937fc55d7a34341
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-04-03 16:26:42 -0300

    estudo diario

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-  echo "Teste 1 ";
-  echo "teste 2";
+
+// $valorgenerico = 20;
+// $nome = $valorgenerico;
+// echo $nome;
+
+// $exemplo = 10;
+// echo $exemplo;
+// $outroExp =& $exemplo;
+// echo $outroExp;
+// $arrayexemplo = [0,1,2,3,4,5,6];
+
+// $exemplo1 =& $arrayexemplo[0];
+
+// $exemplo1 = 100;
+
+// echo $arrayexemplo[0];
+
+
+// $arrayExemplo = [0,1,2,3,4,5,6];
+
+// function quadrado(&$arrayExemplo){
+//   for($i = 0; $i < count($arrayExemplo); $i++){
+//     $arrayExemplo[$i] *= $arrayExemplo[$i];
+//     echo $arrayExemplo[$i];
+//   }
+// };
+// quadrado($arrayExemplo);
+
+// echo $arrayExemplo[6];
+
+
+
+

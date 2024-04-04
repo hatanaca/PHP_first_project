3702d145e1c72de276f2ec0b9e241c17424c46ea
Thiago Hatanaka <thiagohatanaka18@gmail.com>
2024-04-04 13:50:15 -0300

    practicing

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
-<?php
-
-// $valorgenerico = 20;
-// $nome = $valorgenerico;
-// echo $nome;
-
-// $exemplo = 10;
-// echo $exemplo;
-// $outroExp =& $exemplo;
-// echo $outroExp;
-// $arrayexemplo = [0,1,2,3,4,5,6];
-
-// $exemplo1 =& $arrayexemplo[0];
-
-// $exemplo1 = 100;
-
-// echo $arrayexemplo[0];
-
-
-// $arrayExemplo = [0,1,2,3,4,5,6];
-
-// function quadrado(&$arrayExemplo){
-//   for($i = 0; $i < count($arrayExemplo); $i++){
-//     $arrayExemplo[$i] *= $arrayExemplo[$i];
-//     echo $arrayExemplo[$i];
-//   }
-// };
-// quadrado($arrayExemplo);
-
-// echo $arrayExemplo[6];
-
-
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First_php_project</title>
+</head>
+<body>
+    
+</body>
+</html>
